38193a2ef80f9f023a33b8c2f6aa901fd8228e18
XING <zhaoxing8325@gmail.com>
2021-04-28 18:41:43 -0700

    Initial Commit

---
+++ ContactsDemo/ContactsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsDemoApp.swift
+//  ContactsDemo
+//
+//  Created by Xing Zhao on 2021/4/28.
+//
+
+import SwiftUI
+
+@main
+struct ContactsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
